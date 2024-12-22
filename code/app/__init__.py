bd6ccb24f8691452e92ebb252c60c735ddc734bf
rotany <rotany38@gmail.com>
2024-12-22 17:11:20 +0100

    feat: añadiendo ruta con blueprint

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from app.config import Config
 from app.models import db
+from app.routes import image_bp
 
 
     
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(image_bp)
     CORS(app, support_credentials=True)
     app.config.from_object(Config)
     db.init_app(app)
