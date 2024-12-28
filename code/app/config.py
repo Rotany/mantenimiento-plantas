e4b305687f490b7b9653e0583a20841d69a0740f
rotany <rotany38@gmail.com>
2024-12-28 19:03:09 +0100

    feat: agregar variables de entorno

--- code/app/config.py
+++ code/app/config.py
@@ -1,8 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config(object):
-    SECRET_KEY = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://nani:example@localhost:5555/plantas'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SERVER_NAME = os.environ.get('SERVER_NAME')
+
+
+
 
 
